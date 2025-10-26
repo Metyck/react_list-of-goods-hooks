c0952eb28ce08d31d1c55ae22555bac5ef5d6db6
Matvey Turluk <matychencia.ua@gmail.com>
2025-10-26 13:05:08 +0200

    added task solution

---
+++ src/components/getSorted.tsx
@@ -0,0 +1,44 @@
+import { SortType } from '../App';
+
+export function getSortedGoods(
+  currGoods: string[],
+  initialGoods: string[],
+  sortField: SortType | '',
+  reverseField: boolean,
+) {
+  const preparedGoods = currGoods;
+
+  if (sortField === '' && reverseField === false) {
+    return initialGoods;
+  }
+
+  const sortAlphabetically = [...preparedGoods].sort((good1, good2) => {
+    return good1.localeCompare(good2);
+  });
+
+  const sortByLength = [...sortAlphabetically].sort((good1, good2) => {
+    return good1.length - good2.length;
+  });
+
+  if (sortField === SortType.alphabetically) {
+    if (reverseField) {
+      return sortAlphabetically.reverse();
+    }
+
+    return sortAlphabetically;
+  }
+
+  if (sortField === SortType.length) {
+    if (reverseField) {
+      return sortByLength.reverse();
+    }
+
+    return sortByLength;
+  }
+
+  if (reverseField) {
+    return preparedGoods.reverse();
+  }
+
+  return preparedGoods;
+}
